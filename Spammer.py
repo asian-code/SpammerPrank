85e41bb37118056975620f7644c07067015656e8
asian-code <52612730+asian-code@users.noreply.github.com>
2020-04-19 21:42:49 -0400

    Update Spammer.py

--- Spammer.py
+++ Spammer.py
@@ -11,9 +11,7 @@ seconds = .1
 c=0
 keytyper = Controller()
 triggerKey=Key.f4
-#to do list:
-# option 1 spams the whole text file
-# option 2 spames only when holding down the trigger button
+times=0
 
 def textCleanUp(text):
     # remove all \n
@@ -22,9 +20,12 @@ def textCleanUp(text):
     return text
 
 def Menu():
-    print("---Options--------------------------")
-    print(" [0] Spam each word until end of file\n [1] Spam when holding down left 'alt'")
-    print("------------------------------------\n")
+    print("---{0}File Options{1}--------------------------".format(cyan,r))
+    print(" [{0}0{1}] Spam each word in a file. (Uncontrolled)\n [{0}1{1}] Spam when holding down 'f4' key. (Controlled)".format(cyan,r))
+    print("\n---{}Other Options{}-------------------------".format(cyan,r))
+    print(" [{0}2{1}] Spam a word/phrase a number of times\n".format(cyan,r))
+    print("-----------------------------------------\n")
+    
 
 def selectFile():
     try:
@@ -44,13 +45,6 @@ def selectFile():
         raise
 
 def on_press0(key):
-    if key == triggerKey:
-        print("start signal pressed: ")
-    #print('{0} pressed'.format(key))
-
-def on_release0(key):
-    #print('{0} released'.format(key))
-
     # Starting
     if key == triggerKey:
         for i in text:
@@ -60,7 +54,10 @@ def on_release0(key):
             keytyper.release(Key.enter)
             # wait a few mili seconds
             time.sleep(seconds)
+        print(cyan+"[+] Reached end of file"+r)
         return False
+
+def on_release(key):
     # Stop listener
     if key == Key.esc:
         return False
@@ -76,51 +73,50 @@ def on_press1(key):
         if c < len(text)-1:
             c+=1
         else:
+            print(cyan+"[+] Reached end of file"+r)
             return False
-        
-def on_release1(key):
-    # Stop listener
-    if key == Key.esc:
+
+def on_press2(key):
+    if key == triggerKey:
+        for i in range(times):
+            keytyper.type(text)
+            # Enter button
+            keytyper.press(Key.enter)
+            keytyper.release(Key.enter)
+            time.sleep(seconds)
         return False
 
-def startSpam0():
-    print(
-        r +
-        "---{0}Starting{1}----------------------------------\n{0}[!] PRESS 'f4' button to Start, Press 'esc' to quit{1}\n"
-        .format(cyan, r))
-    with Listener(on_press=on_press0, on_release=on_release0) as listener:
-        listener.join()
-
-def startSpam1():
-    print(
-        r +
-        "---{0}Starting{1}----------------------------------\n{0}[!] HOLD 'f4' button to Start, Press 'esc' to quit{1}\n"
-        .format(cyan, r))
-    with Listener(on_press=on_press1, on_release=on_release1) as listener:
+def StartSpam(onPress,onRelease,message):
+    print(r +"---{0}Starting{1}----------------------------------\n{0}{2}{1}\n"
+        .format(cyan, r,message))
+    with Listener(on_press=onPress, on_release=onRelease) as listener:
         listener.join()
 
 def main():
-    global text
+    global text,times
+
+    Menu()
+    userInput=input("[*] Select option: ")
+    userInput=userInput.replace(" ","")
+
+    if userInput=="2":
+        text=input(r+"[*] Enter Text: "+cyan)
+        times=int(input(r+"[*] # of times: "+cyan))
+        StartSpam(on_press2,on_release,"[!] PRESS 'f4' button to Start, Press 'esc' to quit")
+        return 
+
     location = selectFile()
     if location is False:
         return
     # read text data from file
     file = io.open(location,'r',encoding='utf8')
     text = file.read()
-    # print(text)
     file.close()
-    text = textCleanUp(text)
 
-    Menu()
-    userInput=input("[*] Select option: ")
-    userInput=userInput.replace(" ","")
-
+    text = textCleanUp(text)    
     if userInput=="0":
-        startSpam0()
+        StartSpam(on_press0,on_release,"[!] PRESS 'f4' button to Start, Press 'esc' to quit")
     if userInput=="1":
-        startSpam1()
+        StartSpam(on_press1,on_release,"[!] HOLD 'f4' button to Start, Press 'esc' to quit")
     
-    
-
-
 main()
